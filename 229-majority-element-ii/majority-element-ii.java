7dfaf1d12cd1668d0f6791a49a253b39f9dfcefb
Manasvi Singhal <149040276+singhal-menasvee@users.noreply.github.com>
2026-08-05 12:14:09 +0530

    Time: 2 ms (99.96%) | Memory: 52.5 MB (66.53%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,48 +1,54 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-       int count1=0;
-       int maj1=-1;
-       int count2=0;
-       int maj2=-1;
-       for(int i=0;i<nums.length;i++){
-        if(nums[i]==maj1){
-            count1++;
-        }
-        else if(nums[i]==maj2){
-            count2++;
-        }
-        else if(count1==0 && nums[i]!=maj2){
-            maj1=nums[i];
-            count1=1;
-        }
-        else if(count2==0 && nums[i]!=maj1){
-            maj2=nums[i];
-            count2=1;
-        }
-        else{
-            count1--;
-            count2--;
-        }
-       }
 
-       List<Integer> ans= new ArrayList<>();
-       int freq1=0;
-       int freq2=0;
-       for(int num:nums){
-        if(num==maj1){
-            freq1++;
+        int count1 = 0;
+        int count2 = 0;
+
+        int maj1 = -1;
+        int maj2 = -1;
+
+        // Phase 1: Find candidates
+        for (int num : nums) {
+
+            if (count1 == 0 && num != maj2) {
+                maj1 = num;
+                count1 = 1;
+            }
+            else if (count2 == 0 && num != maj1) {
+                maj2 = num;
+                count2 = 1;
+            }
+            else if (num == maj1) {
+                count1++;
+            }
+            else if (num == maj2) {
+                count2++;
+            }
+            else {
+                count1--;
+                count2--;
+            }
         }
-        if(num==maj2){
-            freq2++;
+
+        // Phase 2: Verify candidates
+        int freq1 = 0;
+        int freq2 = 0;
+
+        for (int num : nums) {
+            if (num == maj1)
+                freq1++;
+            else if (num == maj2)
+                freq2++;
         }
-       }
-       if(freq1>nums.length/3){
-        ans.add(maj1);
-       }
-       if(maj2!=maj1 && freq2>nums.length/3){
-        ans.add(maj2);
-       }
-       return ans;
-        
+
+        List<Integer> ans = new ArrayList<>();
+
+        if (freq1 > nums.length / 3)
+            ans.add(maj1);
+
+        if (freq2 > nums.length / 3)
+            ans.add(maj2);
+
+        return ans;
     }
 }
